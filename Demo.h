b96d0f36b068803876dd781c8958ca09ba0ce08a
Lab ESPL Stud18 lab_espl_stud18 <lab_espl_stud18@tueircs-ldb14.int.rcs.ei.tum.de>
2019-05-22 16:39:09 +0200

    ESPL

---
+++ Demo.h
@@ -0,0 +1,29 @@
+/**
+ * Function definitions for the main project file.
+ *
+ * @author: Jonathan Müller-Boruttau,
+ * 			Tobias Fuchs tobias.fuchs@tum.de,
+ * 			Nadja Peters nadja.peters@tum.de (RCS, TUM)
+ */
+
+#ifndef Demo_INCLUDED
+#define Demo_INCLUDED
+
+struct coord {
+	uint8_t x;
+	uint8_t y;
+};
+
+void uartReceive();
+void sendLine(struct coord coord_1, struct coord coord_2);
+void checkJoystick();
+void drawTask();
+void DynamicTask();
+void StaticTask();
+void UpdateDisplay();
+void DrawTaskEx3_2_2();
+void Task3_2_3();
+
+
+
+#endif
